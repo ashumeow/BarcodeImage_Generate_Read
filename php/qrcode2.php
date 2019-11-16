6dc10bc6cf713547a59d92448994f32d92ec41cc
Aswini S <ashumeow@live.com>
2019-11-16 08:49:57 +0300

    Create qrcode2.php

---
+++ php/qrcode2.php
@@ -0,0 +1,11 @@
+<?php
+// INCLUDE THE LIBRARY
+require __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "qrlib.php";
+
+// SAVE TO FILE
+QRcode::png("blah blah blah... save something", "qr-code.png");
+
+// ashumeow
+
+// credits : https://github.com/ashumeow/phpqrcode
+?>
